c5976d9f7f575d46820436beec600c7c8992eb36
Guerson <ultim8p@gmail.com>
2023-03-27 22:23:26 +0200

    Add query for single object.

--- Sources/NoMongo/Collectionable+Query.swift
+++ Sources/NoMongo/Collectionable+Query.swift
@@ -10,7 +10,29 @@ import MongoKitten
 
 public extension DBCollectionable {
     
+    // Finds an existing object and replaces it or creates a new one.
+    // Note that the id of the new object will be taken, if it is different from
+    // the existing one, the id for this object will change.
+    func findAndUpsert(db: MongoDatabase, where query: Document) async throws {
+        let coll = collection(in: db)
+        if let existing = try await Self.findOneOptional(in: db, query: query) {
+            try await existing.delete(in: db)
+        }
+        try await self.save(in: db)
+    }
+}
+
+public extension DBCollectionable {
+    
     // MARK: Single Object
+    
+    func find(in db: MongoDatabase) async throws -> Self {
+        return try await Self.findOne(in: db, id: _id)
+    }
+    
+    func findOptional(in db: MongoDatabase) async throws -> Self? {
+        return try await Self.findOneOptional(in: db, id: _id)
+    }
     
     static func findOne(in db: MongoDatabase, id: ObjectId?) async throws -> Self {
         guard let id = id else { throw DBCollectionableError.missingField("_id") }
