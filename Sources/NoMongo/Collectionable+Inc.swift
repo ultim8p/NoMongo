8ba9424cb0920e2d2f07b9ef5f11d22550fbda59
Guerson <ultim8p@gmail.com>
2023-03-30 01:35:42 +0200

    Added inc & upsert functions.

---
+++ Sources/NoMongo/Collectionable+Inc.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Guerson Perez on 3/29/23.
+//
+
+import Foundation
+import Vapor
+import MongoKitten
+
+public extension DBCollectionable {
+    
+    static func inc(in db: MongoDatabase, updates: [String: Int], query: Document)
+    async throws {
+        let updates: Document = [
+            "$inc": updates,
+            "$set": ["dateUpdated": Date()]
+        ]
+        return try await Self.upsert(in: db, updates: updates, query: query)
+    }
+}
--- Sources/NoMongo/Collectionable+Save.swift
+++ Sources/NoMongo/Collectionable+Save.swift
@@ -15,4 +15,9 @@ public extension DBCollectionable {
         guard let id = _id else { throw DBCollectionableError.missingField("_id") }
         try await collection.upsertEncoded(self, where: "_id" == id)
     }
+    
+    static func upsert(in db: MongoDatabase, updates: Document, query: Document) async throws {
+        let coll = collection(in: db)
+        try await coll.upsert(updates, where: query)
+    }
 }
